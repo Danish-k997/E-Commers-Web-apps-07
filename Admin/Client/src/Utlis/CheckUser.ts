783e81ff27a72e35cad2fc0066f7fb274cd1495c
Danish--k997 <danishkha8321@gmail.com>
2026-04-23 19:53:06 +0530

    fix admin urls

--- Admin/Client/src/Utlis/CheckUser.ts
+++ Admin/Client/src/Utlis/CheckUser.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const checkUser = async () => {
   try {
-    const res = await axios.get("http://localhost:5000/api/auth/profile", {
+    const res = await axios.get("https://e-commers-web-apps-07.onrender.com/api/auth/profile", {
       withCredentials: true
     }); 
 
